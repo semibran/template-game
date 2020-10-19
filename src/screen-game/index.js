6da6d280293a29161ac9a2b8fe5bdf43ad1d2fb0
Brandon Semilla <semibran@gmail.com>
2020-10-18 20:05:55 -0700

    center text

--- src/screen-game/index.js
+++ src/screen-game/index.js
@@ -10,7 +10,13 @@ export function create() {
 }
 
 export function render(game, view) {
+	let viewport = view.viewport
 	let text = view.sprites.Text("Hello world!", { font: "seven" })
-	let node = { image: text }
+	let node = {
+		image: text,
+		x: viewport.width / 2,
+		y: viewport.height / 2,
+		origin: "center"
+	}
 	return [ node ]
 }
